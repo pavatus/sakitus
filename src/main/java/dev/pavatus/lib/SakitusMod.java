33838f7e65363dcab8c195622a62270e2874ea1e
James Hall <james.hall01@outlook.com>
2025-01-20 17:39:22 +0000

    chore: bring over registry code from AIT chore: rename to sakitus.accesswidener build(version): bump

--- src/main/java/dev/pavatus/lib/SakitusMod.java
+++ src/main/java/dev/pavatus/lib/SakitusMod.java
@@ -6,13 +6,15 @@ import org.slf4j.LoggerFactory;
 
 import net.minecraft.util.Identifier;
 
+import dev.pavatus.lib.register.Registries;
+
 public class SakitusMod implements ModInitializer {
     public static final String MOD_ID = "sakitus";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-
+        Registries.getInstance().subscribe(Registries.InitType.COMMON);
     }
 
     public static Identifier id(String path) {
