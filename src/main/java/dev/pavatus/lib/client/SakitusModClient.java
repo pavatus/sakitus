f609eafa0fa309e63af81021f6ad5f3af5eae373
James Hall <james.hall01@outlook.com>
2025-01-20 20:14:37 +0000

    fix(server): ServerLifecycleHooks not being init fix(registry): registry subscriptions being dependent on mod load order build(version): bump v1.0.4

--- src/main/java/dev/pavatus/lib/client/SakitusModClient.java
+++ src/main/java/dev/pavatus/lib/client/SakitusModClient.java
@@ -1,12 +1,15 @@
 package dev.pavatus.lib.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 
 import dev.pavatus.lib.register.Registries;
 
 public class SakitusModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        Registries.getInstance().subscribe(Registries.InitType.CLIENT);
+        ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
+            Registries.getInstance().subscribe(Registries.InitType.CLIENT);
+        });
     }
 }
