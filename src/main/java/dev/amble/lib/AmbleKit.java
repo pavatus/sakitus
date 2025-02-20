c20cbefa7e1bcf84beb0b71e8fde27c9eb725b42
Dr Theo <71725814+DrTheodor@users.noreply.github.com>
2025-02-20 05:20:21 +0300

    fix: remove broken reference to old registry code

--- src/main/java/dev/amble/lib/AmbleKit.java
+++ src/main/java/dev/amble/lib/AmbleKit.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import net.minecraft.util.Identifier;
 
 import dev.amble.lib.api.AmbleKitInitializer;
-import dev.amble.lib.register.AmbleRegistries;
 import dev.amble.lib.util.ServerLifecycleHooks;
 
 public class AmbleKit implements ModInitializer {
@@ -17,7 +16,6 @@ public class AmbleKit implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        AmbleRegistries.getInstance();
         ServerLifecycleHooks.init();
 
         FabricLoader.getInstance().invokeEntrypoints("amblekit-main", AmbleKitInitializer.class,
--- src/main/java/dev/amble/lib/client/AmbleKitClient.java
+++ src/main/java/dev/amble/lib/client/AmbleKitClient.java
@@ -6,6 +6,7 @@ import net.fabricmc.loader.api.FabricLoader;
 import dev.amble.lib.api.AmbleKitClientInitializer;
 
 public class AmbleKitClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
         FabricLoader.getInstance().invokeEntrypoints("amblekit-client", AmbleKitClientInitializer.class,
